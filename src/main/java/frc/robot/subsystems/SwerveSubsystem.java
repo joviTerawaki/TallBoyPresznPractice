3b6fc8abc65e8af56c2d4d9fa8a9f190306df64f
jobo the hobro <jovi.terawaki@gmail.com>
2023-12-04 09:55:10 -1000

    fixed import problem in SwerveSubsystem by rewriting it

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
 import com.pathplanner.lib.auto.AutoBuilder;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
